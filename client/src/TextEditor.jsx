766a106839d6f0c40af31b38199e53b6a10645a7
Anish <anishtharur@gmail.com>
2023-08-13 19:55:34 +0530

    initial changes

---
+++ client/src/TextEditor.jsx
@@ -0,0 +1,18 @@
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { useCallback } from "react";
+
+function TextEditor() {
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper === null) return;
+
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    new Quill(editor, { theme: "snow" });
+  }, []);
+
+  return <div id="container" ref={wrapperRef}></div>;
+}
+
+export default TextEditor;
